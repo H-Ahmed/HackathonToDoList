ad77886fe8623b749d895225e792648e9e69c8df
Ahmed Hesham <eng.ahmed.86313@gmail.com>
2018-02-10 12:01:58 +0200

    Layouts first Commit

--- app/src/main/java/com/example/android/hackathontodolist/ToDoListItem.kt
+++ app/src/main/java/com/example/android/hackathontodolist/ToDoListItem.kt
@@ -1,15 +1,17 @@
 package com.example.android.hackathontodolist
 
+import io.realm.RealmObject
 import java.util.*
 
 /**
  * Created by ahmed on 10/02/2018.
  */
-class ToDoListItem {
+class ToDoListItem : RealmObject(){
 
     var name:String = "";
     var priority:Int =-1;
     var description:String="";
     var date:String="";
     var time: Date? =null;
+
 }
