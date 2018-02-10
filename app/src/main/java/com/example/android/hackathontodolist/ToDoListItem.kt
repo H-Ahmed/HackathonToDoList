696ca8ce9898d2f5d2ea9c9f72bfced9bf2c17aa
ahmed <ahmed.alaa.cis@gmail.com>
2018-02-10 11:30:51 +0200

    added ToDoListModel

---
+++ app/src/main/java/com/example/android/hackathontodolist/ToDoListItem.kt
@@ -0,0 +1,15 @@
+package com.example.android.hackathontodolist
+
+import java.util.*
+
+/**
+ * Created by ahmed on 10/02/2018.
+ */
+class ToDoListItem {
+
+    var name:String = "";
+    var priority:Int =-1;
+    var description:String="";
+    var date:String="";
+    var time: Date? =null;
+}
